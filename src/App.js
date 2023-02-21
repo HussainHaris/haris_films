9f998525b53f583ce9496cadcf979f8735af4321
HussainHaris <hussainharis04@gmail.com>
2023-02-21 13:07:10 -0500

    navbar initial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
 import { Navbar } from './components';
-
 import { About, Header, Footer, Services } from './containers';
+import './app.css';
+import './index.css';
 
 const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
+        <div className="wrapper">
         <Navbar />
         <Header />
         <About />
         <Services />
         <Footer />
+        </div>
       </div>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  const Menu = () => (
+    <>                
+      <p><a href="#home">Home</a></p>
+      <p><a href="#work">Work</a></p>
+      <p><a href="#pricing">Pricing</a></p>
+      <p><a href="#contact">Contact</a></p>    
+    </>
+  )
+
+  return (
+    <div className="haris_films__navbar">
+      <div className="haris_films__navbar-links">
+        <div className="haris_films__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="haris_films__navbar-links_container">
+          <Menu />
+        </div>
+        <div className="haris_films__navbar-menu">
+          {toggleMenu 
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} /> }
+
+          {toggleMenu && (
+            <div className="haris_films__navbar-menu_container scale-up-center">
+              <div className="haris_films__navbar-menu_container-links">
+                <Menu />
+              </div>
+            </div>
+          )}
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
