39d409d62ab705903ebc5387f7830fa8dc852f18
HussainHaris <hussainharis04@gmail.com>
2023-02-23 23:04:26 -0500

    implemented video asset for header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './header.css';
+import test from "../../assets/test.mp4";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="haris_films__header">
+      <div className="haris_fims__header-content">
+        <div className="haris_films__header-content_video">
+          <video autoPlay loop muted>
+            <source src={test} type="video/mp4"/>
+          </video>
+        </div>
+      </div>
     </div>
   )
 }
