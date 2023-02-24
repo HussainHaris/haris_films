dc3159439f9c3cc2338c32e0446880debd311eb2
HussainHaris <hussainharis04@gmail.com>
2023-02-24 09:46:29 -0500

    stylized header for desktop

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,14 @@ import test from "../../assets/test.mp4";
 const Header = () => {
   return (
     <div className="haris_films__header">
-      <div className="haris_fims__header-content">
+      <div className="haris_films__header-content">
+        <div className="haris_films__header-content-container">
+          <h1 className="gradient_text">Haris Films</h1>
+          <p>Bringing your moments to life, frame by frame.</p>
+          <div className="haris_films__header-content-btns">
+            <button type="button">View Portfolio</button>
+          </div>
+        </div>
         <div className="haris_films__header-content_video">
           <video autoPlay loop muted>
             <source src={test} type="video/mp4"/>
